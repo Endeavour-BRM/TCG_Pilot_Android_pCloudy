1825d508c0fbad1f5cb023f8ebc52b7945343144
Aanjan Hari <aanjan.hari@estblr.com>
2017-04-04 12:34:32 +0530

    push

---
+++ appiumtest/src/appiumtest/appiumtest.java
@@ -0,0 +1,87 @@
+package appiumtest;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.ssts.pcloudy.ConnectError;
+import com.ssts.pcloudy.Connector;
+import com.ssts.pcloudy.dto.booking.BookingDtoDevice;
+import com.ssts.pcloudy.dto.device.MobileDevice;
+import com.ssts.pcloudy.dto.file.PDriveFileDTO;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class appiumtest {
+	 protected static WebDriver driver;
+	public void InstallApp() throws IOException, ConnectError, InterruptedException{
+		Connector pCloudyCONNECTOR = new Connector("https://fonebooth.techendeavour.com");
+    	
+    	String authToken = pCloudyCONNECTOR.authenticateUser("aanjan.hari@techendeavour.com", "wrw6sfqq3wdpdwmy3zvzn8ys");
+    	ArrayList selectedDevices = new ArrayList();
+    	MobileDevice[] devices = pCloudyCONNECTOR.getDevices(authToken,1,"android",true);
+    	
+    	for(int i = 0; i<devices.length;i++){
+    		System.out.println(devices[i].full_name);
+    		System.out.println(devices[i].id);
+    		System.out.println(devices[i].model);
+    		System.out.println(devices[i].platform);
+    		System.out.println(devices[i].version);
+    		System.out.println(devices[i].manufacturer);
+    		System.out.println(devices[i].display_name);
+    	}
+    	// Populate the selected Devices here
+    	//selectedDevices.add(pCloudyCONNECTOR.getDevices(authToken,1,"android",true));
+    	
+    	selectedDevices.add(MobileDevice.getNew("Samsung_GalaxyS4_Android_5.0.1", 19, "GalaxyS4", "Galaxy S4", "android", "5.0.1", "Samsung"));
+
+
+    	// Book the selected devices in pCloudy
+
+    	BookingDtoDevice[] bookedDevicesIDs = pCloudyCONNECTOR.bookDevicesForAppium(authToken, selectedDevices, 1, "friendlySessionName");;
+    	System.out.println("Devices booked successfully");
+    	
+		
+	
+
+    	// Upload apk in pCloudy
+    	PDriveFileDTO pDriveFile = pCloudyCONNECTOR.uploadApp(authToken, new File("d:/Users/aanjan.hari/Downloads/Spinner-release.apk"));
+    	System.out.println("apk file uploaded successfully");
+    	pCloudyCONNECTOR.initAppiumHubForApp(authToken, pDriveFile);
+
+    	// Get the endpoint from pCloudy
+    	URL endpoint = pCloudyCONNECTOR.getAppiumEndpoint(authToken);
+    	System.out.println("Appium Endpoint:" + endpoint);
+    	
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //capabilities.setCapability("appium-version", "1.0");
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("platformVersion", "6.0.1");
+        capabilities.setCapability("deviceName", bookedDevicesIDs[0].capabilities.deviceName);
+        capabilities.setCapability("autoAcceptAlerts", true);
+        capabilities.setCapability("autoDismissAlerts", true);
+        //capabilities.setCapability("app", pDriveFile.file.);
+        //capabilities.setCapability("appPackage", "com.flipkart.android");
+        //capabilities.setCapability("appActivity", "com.flipkart.android.SplashActivity");
+        driver = new AndroidDriver(endpoint, capabilities);
+
+        
+        //driver.findElement(By.xpath("//android.widget.Button")).click();
+        //String windowhandle = driver.getWindowHandle();
+        //driver.switchTo().alert().accept();
+        //driver.switchTo().window(windowhandle);
+
+	}
+	
+	public void selectElement(String element){
+        driver.findElement(By.className("android.widget.CheckedTextView")).click();
+        driver.findElement(By.name(element)).click();
+	}
+
+}
---
+++ appiumtest/src/test/java/TestSpinner.java
@@ -0,0 +1,38 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+
+import java.io.IOException;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.ssts.pcloudy.ConnectError;
+
+import appiumtest.appiumtest;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestSpinner {
+
+
+	@Test
+	public void t1InstallStartApp() throws IOException, ConnectError, InterruptedException {
+		appiumtest cAppium = new appiumtest();
+		cAppium.InstallApp();
+	}
+	
+	@Test
+	public void t2SelectEarth(){
+		appiumtest cAppium = new appiumtest();
+		cAppium.selectElement("Earth");
+	}
+	
+	@Test
+	public void t3SelectMars(){
+		appiumtest cAppium = new appiumtest();
+		cAppium.selectElement("Mars");
+	}
+
+}
