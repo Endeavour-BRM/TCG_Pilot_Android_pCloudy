3cf38a9ccc2c405d709a4bf925b110a479934e68
Aanjan Hari <aanjan.hari@estblr.com>
2017-05-29 19:02:38 +0530

    push

--- appiumtest/src/appiumtest/appiumtest.java
+++ appiumtest/src/appiumtest/appiumtest.java
@@ -67,7 +67,7 @@ public class appiumtest {
         //capabilities.setCapability("appium-version", "1.0");
         capabilities.setCapability("platformName", "android");
         capabilities.setCapability("platformVersion", "5.0.1");
-		System.out.println(bookedDevicesIDs[0].capabilities.deviceName)
+		System.out.println(bookedDevicesIDs[0].capabilities.deviceName);
         capabilities.setCapability("deviceName", bookedDevicesIDs[0].capabilities.deviceName);
         capabilities.setCapability("autoAcceptAlerts", true);
         capabilities.setCapability("autoDismissAlerts", true);
